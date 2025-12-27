a3a0543f58d8d88429d9cde06c1c1ff5f15d769b
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-27 12:18:57 -0600

    Lía Auto-Dev: gba_core/main.c

--- gba_core/main.c
+++ gba_core/main.c
@@ -1,28 +1,18 @@
 #include "kaia_gba.h"
-#define NEGRO 0x0000
-#define AZUL  0x7C00
 
-// --- IMPLEMENTACIÓN DE FUNCIONES (Deben estar aquí) ---
-void vid_vsync() {
-    while(REG_VCOUNT >= 160);
-    while(REG_VCOUNT < 160);
-}
-
-void dibujarJugador(int x, int y, int color) {
-    for(int i = 0; i < 16; i++) {
-        for(int j = 0; j < 16; j++) {
-            VRAM[(y + j) * 240 + (x + i)] = color;
-        }
-    }
-}
-
-// --- MAIN ---
 int main() {
-    REG_DISPCNT = MODE_3 | BG2_ENABLE;
-    int x = 120, y = 80;
-    while(1) {
+    // Limpieza inicial
+    for(int i=0; i<38400; i++) VRAM[i] = NEGRO;
+    int x = 80;
+    int y = 60;
+    while (1) {
+        dibujarJugador(x, y, NEGRO); // Borrar viejo
+        if (!(REG_KEYINPUT & KEY_RIGHT)) x++;
+        if (!(REG_KEYINPUT & KEY_LEFT))  x--;
+        if (!(REG_KEYINPUT & KEY_UP))    y--;
+        if (!(REG_KEYINPUT & KEY_DOWN))  y++;
+        dibujarJugador(x, y, AZUL);  // Pintar nuevo
         vid_vsync();
-        dibujarJugador(x, y, AZUL);
     }
     return 0;
 }
