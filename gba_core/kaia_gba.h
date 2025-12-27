27c6dad14fb36ac12a6f56279a2e0f767a5fd617
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-27 11:09:43 -0600

    Lía Auto-Dev: gba_core/kaia_gba.h

--- gba_core/kaia_gba.h
+++ gba_core/kaia_gba.h
@@ -1,3 +1,18 @@
+typedef unsigned short u16;
+typedef unsigned int u32;
+
 #define REG_DISPCNT (*(volatile u16*)0x04000000)
 #define REG_KEYINPUT (*(volatile u16*)0x04000130)
 #define VRAM ((volatile u16*)0x06000000)
+
+#define MODE_3 0x0003
+#define BG2_ENABLE 0x0400
+
+#define KEY_A 0x0001
+#define KEY_B 0x0002
+#define KEY_SELECT 0x0004
+#define KEY_START 0x0008
+#define KEY_RIGHT 0x0010
+#define KEY_LEFT 0x0020
+#define KEY_UP 0x0040
+#define KEY_DOWN 0x0080
