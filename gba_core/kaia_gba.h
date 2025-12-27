fef1de7ee5a17a629d9e7e1527152b1380354486
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-27 12:15:51 -0600

    Lía Auto-Dev: gba_core/kaia_gba.h

--- gba_core/kaia_gba.h
+++ gba_core/kaia_gba.h
@@ -1,21 +1,26 @@
 #ifndef KAIA_GBA_H
 #define KAIA_GBA_H
 
-// Definiciones de tipos
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
+// Definiciones de registros
+#define REG_DISPCNT  *(volatile unsigned short *)0x04000000
+#define REG_KEYINPUT *(volatile unsigned short *)0x04000130
+#define REG_KEYCNT  *(volatile unsigned short *)0x04000132
 
-// Registros
-#define REG_DISPCNT *(volatile u16*)0x04000000
-#define REG_VCOUNT *(volatile u16*)0x04000006
-#define REG_KEYINPUT *(volatile u16*)0x04000130
+// Definiciones de teclas
+#define KEY_A      0x0001
+#define KEY_B      0x0002
+#define KEY_SELECT 0x0004
+#define KEY_START  0x0008
+#define KEY_RIGHT  0x0010
+#define KEY_LEFT   0x0020
+#define KEY_UP     0x0040
+#define KEY_DOWN   0x0080
 
-// Constantes MODO 3
-#define MODE_3 0x0003
-#define BG2_ENABLE 0x0400
+// Definiciones de memoria
+#define VRAM       ((volatile unsigned short *)0x06000000)
 
-// Puntero VRAM
-#define VRAM ((volatile u16*)0x06000000)
+// Funciones
+void vid_vsync();
+void dibujarJugador();
 
-#endif  // KAIA_GBA_H
+#endif
