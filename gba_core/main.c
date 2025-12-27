731c517625a0ff0f6cd74093c04e4a236101ab05
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-27 11:58:39 -0600

    Lía Auto-Dev: gba_core/main.c

--- gba_core/main.c
+++ gba_core/main.c
@@ -1,44 +1,42 @@
 #include "kaia_gba.h"
 
-// Función para sincronizar con la velocidad vertical
-void vid_vsync() {
-    while (REG_VCOUNT < 160);
-}
-
-// Función helper para dibujar un jugador
-void dibujarJugador(int x, int y, u16 color) {
-    for (int i = 0; i < 16; i++) {
-        for (int j = 0; j < 16; j++) {
-            VRAM[(y + i) * 240 + x + j] = color;
-        }
-    }
+void vid_vsync() { 
+    while(REG_VCOUNT >= 160); 
+    while(REG_VCOUNT < 160); 
 }
 
 int main() {
-    // Inicializa modo de video
-    REG_DISPCTL = MODE_3 | BG2_ENABLE;
+    REG_DISPCNT = MODE_3 | BG2_ENABLE;
+    int x = 0;
+    int y = 0;
+    int old_x = x;
+    int old_y = y;
+    int color_azul = RGB(0, 0, 255);
+    int color_negro = RGB(0, 0, 0);
 
-    int x = 100;
-    int y = 100;
-
-    while (1) {
-        // Paso A (Borrar)
-        dibujarJugador(x, y, 0);
-
-        // Paso B (Input)
+    while(1) {
+        vid_vsync();
         if (!(REG_KEYINPUT & KEY_RIGHT)) {
-            x += 1;
-            if (x > 235) {
+            old_x = x;
+            x++;
+            if(x > 239) {
                 x = 0;
             }
         }
 
-        // Paso C (Dibujar)
-        dibujarJugador(x, y, 0x001F); // Azul
+        // Limpia la posición vieja
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                plotPixel(old_x + i, old_y + j, color_negro);
+            }
+        }
 
-        // Paso D (Sincronizar)
-        vid_vsync();
+        // Pinta la nueva posición
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                plotPixel(x + i, y + j, color_azul);
+            }
+        }
     }
-
     return 0;
 }
