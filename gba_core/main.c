cce96ac143cbd6f0c55a042624852c5fd7e63a64
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-27 12:18:43 -0600

    Lía Auto-Dev: gba_core/main.c

--- gba_core/main.c
+++ gba_core/main.c
@@ -1,21 +1,28 @@
 #include "kaia_gba.h"
+#define NEGRO 0x0000
+#define AZUL  0x7C00
 
-int main() {
-    // Inicializar el modo de video
-    REG_DISPCNT = 0x0400;
+// --- IMPLEMENTACIÓN DE FUNCIONES (Deben estar aquí) ---
+void vid_vsync() {
+    while(REG_VCOUNT >= 160);
+    while(REG_VCOUNT < 160);
+}
 
-    while (1) {
-        // Esperar a que se complete el ciclo de video
-        vid_vsync();
-
-        // Dibujar al jugador
-        dibujarJugador();
-
-        // Revisar las teclas presionadas
-        if (REG_KEYCNT & KEY_RIGHT) {
-            // Mover al jugador a la derecha
+void dibujarJugador(int x, int y, int color) {
+    for(int i = 0; i < 16; i++) {
+        for(int j = 0; j < 16; j++) {
+            VRAM[(y + j) * 240 + (x + i)] = color;
         }
     }
+}
 
+// --- MAIN ---
+int main() {
+    REG_DISPCNT = MODE_3 | BG2_ENABLE;
+    int x = 120, y = 80;
+    while(1) {
+        vid_vsync();
+        dibujarJugador(x, y, AZUL);
+    }
     return 0;
 }
