71cbafb90f9cf07c22a348de376ff141a8aea856
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-27 12:08:23 -0600

    Lía Auto-Dev: gba_core/main.c

--- gba_core/main.c
+++ gba_core/main.c
@@ -1,42 +1,40 @@
 #include "kaia_gba.h"
+#define NEGRO   0x0000
+#define AZUL    0x7C00  // (31 << 10)
 
-void vid_vsync() { 
-    while(REG_VCOUNT >= 160); 
-    while(REG_VCOUNT < 160); 
+void vid_vsync() {
+    while(REG_VCOUNT >= 160); // Esperar a que termine VBlank
+    while(REG_VCOUNT < 160);  // Esperar a que empiece VBlank
+}
+
+void dibujarJugador(int x, int y, u16 color) {
+    // Dibuja un cuadrado de 16x16
+    for(int i = 0; i < 16; i++) {
+        for(int j = 0; j < 16; j++) {
+            // Escribe directamente en VRAM
+            VRAM[(y + j) * 240 + (x + i)] = color;
+        }
+    }
 }
 
 int main() {
+    // Configurar Pantalla
     REG_DISPCNT = MODE_3 | BG2_ENABLE;
-    int x = 0;
-    int y = 0;
-    int old_x = x;
-    int old_y = y;
-    int color_azul = RGB(0, 0, 255);
-    int color_negro = RGB(0, 0, 0);
-
-    while(1) {
+    int posX = 120;
+    int posY = 80;
+    // Bucle Principal
+    while (1) {
+        // A. Borrar posición vieja (Pintar NEGRO)
+        dibujarJugador(posX, posY, NEGRO);
+        // B. Leer Inputs (Recuerda: 0 es presionado)
+        if (!(REG_KEYINPUT & KEY_RIGHT)) posX++;
+        if (!(REG_KEYINPUT & KEY_LEFT))  posX--;
+        if (!(REG_KEYINPUT & KEY_UP))    posY--;
+        if (!(REG_KEYINPUT & KEY_DOWN))  posY++;
+        // C. Dibujar posición nueva (Pintar AZUL)
+        dibujarJugador(posX, posY, AZUL);
+        // D. Sincronizar
         vid_vsync();
-        if (!(REG_KEYINPUT & KEY_RIGHT)) {
-            old_x = x;
-            x++;
-            if(x > 239) {
-                x = 0;
-            }
-        }
-
-        // Limpia la posición vieja
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++) {
-                plotPixel(old_x + i, old_y + j, color_negro);
-            }
-        }
-
-        // Pinta la nueva posición
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++) {
-                plotPixel(x + i, y + j, color_azul);
-            }
-        }
     }
     return 0;
 }
