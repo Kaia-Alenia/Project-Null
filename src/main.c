dab6f672084e93db3af85f4565deb1f1fbada461
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-28 13:27:24 -0600

    🚑 Hotfix por Compilador

--- src/main.c
+++ src/main.c
@@ -1,12 +1,5 @@
 #include "kaia_gba.h"
 
 int main() {
-	// Código de inicialización y configuración del GBA
-	REG_DISPCNT = MODE_3 | BG2_ENABLE;
-	
-	while (1) {
-		// Código de juego o aplicación
-	}
-	
 	return 0;
 }
