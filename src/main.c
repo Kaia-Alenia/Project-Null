8f9d3534b5bac6e6c81bada1a201e3ff91e637a8
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-28 17:53:33 -0600

    🚑 Fix Quirúrgico por Lia

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
-volatile unsigned short* oam = (volatile unsigned short*)0x07000000;
-oam[0] = 0x0000; // Y pos
-oam[1] = 0x0000; // X pos
-oam[2] = 0x0000; // Tile index
-oam[3] = 0x0000; // Attributes
+#include <stdint.h>
+
+int main() {
+    volatile unsigned short* oam = (volatile unsigned short*)0x07000000;
+    oam[0] = 0x0000; // Y pos
+    oam[1] = 0x0000; // X pos
+    oam[2] = 0x0000; // Tile index
+    oam[3] = 0x0000; // Attributes
+    while(1);
+}
