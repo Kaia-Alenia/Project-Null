8072c17d2e7c69fbefad13a5deea641a7dd013e7
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-28 13:18:11 -0600

    🚑 Hotfix por Compilador

--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
-#include "../include/kaia_gba.h"
+#include "kaia_gba.h"
+
+int main() {
+    // Inicialización de la consola
+    *(volatile u16*)0x04000000 = MODE_3 | BG2_ENABLE;
+
+    while (1) {
+        // Bucle principal
+    }
+
+    return 0;
+}
