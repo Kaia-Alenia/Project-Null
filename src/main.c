862170789b0694d14492ae8650376e4e1b8fd38c
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-28 13:28:21 -0600

    🚑 Hotfix por Compilador

--- src/main.c
+++ src/main.c
@@ -1,18 +1,5 @@
 #include "kaia_gba.h"
 
 int main() {
-    // Inicializar la pantalla
-    *(volatile u16*)0x04000000 = MODE_3 | BG2_ENABLE;
-
-    while (1) {
-        // Leer la entrada del teclado
-        u16 keys = *(volatile u16*)0x04000130;
-
-        // Verificar si se presiona la tecla A
-        if (keys & KEY_A) {
-            // Acción cuando se presiona la tecla A
-        }
-    }
-
-    return 0;
+	return 0;
 }
