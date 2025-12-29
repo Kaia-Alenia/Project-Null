ec3940a17ab7ae0225c636c368699d202e1cebd1
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2025-12-28 18:02:32 -0600

    🚑 Fix Quirúrgico por Lia

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
-clude <stdint.h>
+#include <stdint.h>
 
 // Definiciones de Hardware (Directas para evitar errores de headers faltantes)
 typedef unsigned short u16;
-#define REG_DISPCNT  ((volatile u16)0x04000000)
+#define REG_DISPCNT  (*(volatile u16*)0x04000000)
 #define VRAM         ((volatile u16*)0x06000000)
 #define MODE_3       0x0003
 #define BG2_ENABLE   0x0400
